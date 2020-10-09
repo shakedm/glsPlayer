c4969714891af5d25d39d46621e39955bc72b72e
shakedm <shakedmigdal@campus.technion.ac.il>
2020-10-09 16:30:50 +0300

    declaring const's and importing jquery
    first attempt at importing the jQuery

---
+++ player.js
@@ -0,0 +1,9 @@
+
+const googleUrl = 'https://www.google.com/';
+const JSONPUrl ='https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&amp;refresh=true&amp;env=dev&amp;type=startPanel&amp;vars%5Btype%5D=startPanel&amp;sid=none&amp;_=1582203987867';
+
+function jQueryImport(){
+    let script = document.createElement('script');
+    script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";
+    document.body.append(script);
+}
