d92632a8393253631a00e44e77b9d8ddb6c0b542
shakedm <shakedmigdal@campus.technion.ac.il>
2020-10-09 21:46:27 +0300

    Update glsWrapper.js

--- glsWrapper.js
+++ glsWrapper.js
@@ -11,4 +11,26 @@ class glsWrapper{
         const { structure } = this._data;
         return structure;
     }
+
+    get tiplates () {
+        const { tiplates } = this._data;
+        return tiplates;
+    }
+
+    get css () {
+        const { css } = this._data;
+        return css;
+        // might need to parse the css?
+    }
+
+    get settings () {
+        const { settings } = this._data;
+        return settings;
+    }
+
+    get steps () {
+        const { steps } = this.structure;
+        return this._steps;
+        // check this
+    }
 }
