cbd2313b8bf6ade70e24b4da567553f523f1851b
shakedm <shakedmigdal@campus.technion.ac.il>
2020-10-14 22:52:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-function startMyRunnig () {
+function startMyRunning () {
     const myNewScript = document.createElement('script');
     myNewScript.type = 'module';
     myNewScript.src = 'https://peaceful-joliot-b94953.netlify.app/player.js';
     document.body.appendChild(myNewScript);
   }
-  startMyRunnig();
+  startMyRunning();
   var guideRes = window.guideRes;
